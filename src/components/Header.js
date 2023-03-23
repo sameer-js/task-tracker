99645ebf4400b760ea11143c4862d018076a688c
Sameer Jung Subedi <meersj@gmail.com>
2023-03-23 10:04:04 +0545

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+// const hStyle = {
+//     color: "red",
+//     backgroundColor: "gold"
+// };
+
+export default Header;
